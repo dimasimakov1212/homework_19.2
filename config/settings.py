936625ad348d4bdcda688093c5d12a6a6f51079a
DimaSimakov <dimarodeo@gmail.com>
2023-08-01 15:52:43 +0700

    created django ORM

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework_19',
+        'USER': 'postgres',
+        'PASSWORD': 'dima123',
+        'HOST': '127.0.0.1'
     }
 }
 
