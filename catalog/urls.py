dfaa6818533504bda548866abf89431945dc4c7c
DimaSimakov <dimarodeo@gmail.com>
2023-08-06 16:07:19 +0700

    add urls in inc_catalog_menu.html for main menu

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from catalog.views import index, contact, product_show
+from main.apps import MainConfig
+
+app_name = MainConfig.name
 
 urlpatterns = [
-    path('', product_show),
+    path('', product_show, name='home'),
     # path('', index),
-    path('contacts/', contact),
+    path('contacts/', contact, name='contacts'),
 
 ]
