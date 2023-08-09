a07f05a7653de336f4d15c6de2f370c96f4f7a21
DimaSimakov <dimarodeo@gmail.com>
2023-08-09 18:38:42 +0700

    changed controllers from FBV to CBV

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -5,13 +5,6 @@ from config import settings
 register = template.Library()
 
 
-# @register.filter()
-# def my_media(val):
-#     if val:
-#         return f'/media/{val}'
-#
-#     return '#'
-
 @register.filter()
 def my_media(value):
     return f"{settings.MEDIA_URL}{value}"
