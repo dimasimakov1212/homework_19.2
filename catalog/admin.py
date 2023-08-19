adb10822c340ece5a6a851feb95fb7a6a2c6a85f
dimas <dimarodeo@gmail.com>
2023-08-19 20:21:55 +0700

    add BlogAdmin for model Blog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Blog
 
 
 @admin.register(Product)
@@ -19,3 +19,13 @@ class CategoryAdmin(admin.ModelAdmin):
     Описывает параметры для вывода таблицы категорий товаров в админку
     """
     list_display = ('id', 'category_name',)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    """
+    Описывает параметры для вывода таблицы статей блогав админку
+    """
+    list_display = ('blog_title', 'blog_date_creation', 'blog_views_count', 'blog_is_active',)
+    list_filter = ('blog_title',)
+    search_fields = ('blog_title', 'blog_text',)
