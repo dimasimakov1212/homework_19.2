5614c9c786cb282f5a5649e8de797ae09b66d2a1
dimas <dimarodeo@gmail.com>
2023-08-19 19:29:13 +0700

    created new model Blog in app catalog

--- catalog/models.py
+++ catalog/models.py
@@ -47,3 +47,25 @@ class Category(models.Model):
         verbose_name = 'Категория'  # Настройка для наименования одного объекта
         verbose_name_plural = 'Категории'  # Настройка для наименования набора объектов
         ordering = ('category_name',)  # сортировка по наименованию
+
+
+class Blog(models.Model):
+    """
+    Класс для ведения блога
+    """
+    blog_title = models.CharField(max_length=150, verbose_name='Заголовок')
+    blog_text = models.CharField(max_length=150, verbose_name='Содержимое')
+    blog_preview = models.ImageField(upload_to='catalog/', verbose_name='Превью', **NULLABLE)
+    blog_slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
+    blog_date_creation = models.DateField(auto_now=False, auto_now_add=True, verbose_name='Дата создания')
+    blog_is_active = models.BooleanField(default=True, verbose_name='Опубликовано')
+    blog_views_count = models.IntegerField(default=0, verbose_name='Просмотры')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.blog_title}'
+
+    class Meta:
+        verbose_name = 'Статья'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Статьи'  # Настройка для наименования набора объектов
+        ordering = ('blog_title',)  # сортировка по наименованию
