db47e2c5468c89fb97d695e09f01dccc4bfc8db4
DimaSimakov <dimarodeo@gmail.com>
2023-08-25 22:36:11 +0700

    add valid method in ProductForm to check wrong words

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,6 +7,7 @@ class ProductForm(forms.ModelForm):
     """
      Создает форму для создания товара
     """
+
     class Meta:
         """
         Определяет параметры формы
@@ -15,19 +16,29 @@ class ProductForm(forms.ModelForm):
 
         exclude = ('date_creation', 'date_changing')  # выводит в форму все поля, кроме указанных
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #
-    #     for field_name, field in self.fields.items():
-    #         field.widget.attrs['class'] = 'form-control'
-    #
-    # def clean_email(self):
-    #     """
-    #     Проверяет корректность ввода email
-    #     """
-    #     cleaned_data = self.cleaned_data['email']
-    #
-    #     if 'sky.pro' not in cleaned_data:
-    #         raise forms.ValidationError('Почта должна относиться к учебному заведению')
-    #
-    #     return cleaned_data
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        """
+        Проверяет корректность ввода названия товара
+        """
+        # задаем список запрещенных к использованию слов
+        stop_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        cleaned_data = super().clean()
+
+        name = self.cleaned_data['product_name']
+        description = self.cleaned_data['product_description']
+
+        for item in stop_list:
+            if item in name.lower():
+                raise forms.ValidationError(f'Слово "{item}" запрещено к использованию, выберите другое')
+
+            if item in description.lower():
+                raise forms.ValidationError(f'Слово "{item}" запрещено к использованию, выберите другое')
+
+        return cleaned_data
