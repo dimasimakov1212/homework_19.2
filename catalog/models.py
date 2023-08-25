66bbab9e74e887ad53174c013ad6436cd54b6790
DimaSimakov <dimarodeo@gmail.com>
2023-08-26 00:44:38 +0700

    changed models Product and Category

--- catalog/models.py
+++ catalog/models.py
@@ -6,6 +6,25 @@ from django.db.models import SET_DEFAULT, SET_NULL
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    """
+    Класс для создания категорий товаров
+    """
+    category_name = models.CharField(max_length=150, verbose_name='Категория')
+    category_description = models.CharField(max_length=150, verbose_name='Описание')
+
+    # is_active = models.BooleanField(default=True, verbose_name='учится')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'Категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Категории'  # Настройка для наименования набора объектов
+        ordering = ('category_name',)  # сортировка по наименованию
+
+
 class Product(models.Model):
     """
     Класс для создания товаров
@@ -13,7 +32,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=150, verbose_name='Наименование')
     product_description = models.CharField(max_length=150, verbose_name='Описание')
     product_preview = models.ImageField(upload_to='catalog/', verbose_name='Превью', **NULLABLE)
-    product_category = models.CharField(max_length=150, verbose_name='Категория')
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     product_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     date_creation = models.DateField(auto_now=False, auto_now_add=True, verbose_name='Дата создания')
     date_changing = models.DateField(auto_now=True, auto_now_add=False, verbose_name='Дата последнего изменения')
@@ -28,25 +47,6 @@ class Product(models.Model):
         verbose_name = 'Товар'  # Настройка для наименования одного объекта
         verbose_name_plural = 'Товары'  # Настройка для наименования набора объектов
         ordering = ('product_name',)  # сортировка по наименованию
-
-
-class Category(models.Model):
-    """
-    Класс для создания категорий товаров
-    """
-    category_name = models.CharField(max_length=150, verbose_name='Наименование')
-    category_description = models.CharField(max_length=150, verbose_name='Описание')
-
-    # is_active = models.BooleanField(default=True, verbose_name='учится')
-
-    def __str__(self):
-        # Строковое отображение объекта
-        return f'{self.category_name}'
-
-    class Meta:
-        verbose_name = 'Категория'  # Настройка для наименования одного объекта
-        verbose_name_plural = 'Категории'  # Настройка для наименования набора объектов
-        ordering = ('category_name',)  # сортировка по наименованию
 
 
 class Blog(models.Model):
