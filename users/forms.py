8263e3e38009123e8c3b226e62115781f808e4b6
DimaSimakov <dimarodeo@gmail.com>
2023-08-30 17:59:24 +0700

    add user_form.html, controller for users profile

--- users/forms.py
+++ users/forms.py
@@ -11,3 +11,28 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserProfileForm(UserChangeForm):
+    """
+    Форма профиля пользователя
+    """
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Позволяет не выводить в профиле поле пароля
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.HiddenInput()
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
 
@@ -15,3 +15,18 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+
+class ProfileView(UpdateView):
+    """
+    Контроллер профиля пользователя
+    """
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        """
+        Позволяет делать необязательным передачу pk объекта
+        """
+        return self.request.user
