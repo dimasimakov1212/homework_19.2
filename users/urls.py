a87c2bce250dac1683368db199cf0d39abf0b650
DimaSimakov <dimarodeo@gmail.com>
2023-09-01 21:01:26 +0700

    add verification for new users by email

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, generate_new_password
+from users.views import RegisterView, ProfileView, generate_new_password, UserConfirmEmailView, \
+    UserConfirmationSentView, UserConfirmedView, UserConfirmationFailView
 
 app_name = UsersConfig.name
 
@@ -13,4 +14,8 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/genpassword/', generate_new_password, name='generate_new_password'),
+    path('email_confirmation_sent/', UserConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email_confirmed/', UserConfirmedView.as_view(), name='email_confirmed'),
+    path('email_confirmation_failed/', UserConfirmationFailView.as_view(), name='email_confirmation_failed'),
     ]
