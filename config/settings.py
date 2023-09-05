3bf2c4c39a5b766170e6e5459f3d0fd3bfbb5eeb
DimaSimakov <dimarodeo@gmail.com>
2023-09-05 20:44:01 +0700

    add redis package

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,13 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = 'dimsim1212@yandex.ru'
 EMAIL_HOST_PASSWORD = 'ajnyqbiehkhikqvj'
 EMAIL_USE_SSL = True
+
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
+    }
